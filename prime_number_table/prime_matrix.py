427e818729976df375ab90636f7551061c460148
Leonardo da Mata <barroca@gmail.com>
2018-09-12 01:41:05 +0100

    Add Class to print return matrix

--- prime_number_table/prime_matrix.py
+++ prime_number_table/prime_matrix.py
@@ -1,11 +1,14 @@
 from prime_number_table.exceptions import NotIntegerError, NotSupported
 from prime_number_table.prime_number_generator import PrimeNumberGenerator
+import numpy
 
 
 class PrimeMatrix():
     def __init__(self):
         self.prime_list = []
+        self.result_matrix = []
         self.prime_number_generator = PrimeNumberGenerator()
+        # we toggle optimization feature flag since we want to run faster
         self.prime_number_generator.toggle_feature_flag_optimize_prime_check()
 
 
@@ -17,4 +20,18 @@ class PrimeMatrix():
             raise NotSupported()
         if primes > 0:
             self.prime_list = self.prime_number_generator.get_first_primes(primes)
-        return self.prime_list
+            # we first initialize a matrix with empty values
+            self.result_matrix = numpy.zeros((primes, primes))
+
+            # we initialize first row and column with the first primes
+            for x in range(primes):
+                self.result_matrix[x][0] = self.prime_list[x]
+                self.result_matrix[0][x] = self.prime_list[x]
+            # we iterate over the diagonal and fill the multiplication values
+            for x in range(1, primes):
+                for y in range(x, primes):
+                    result = self.prime_list[x] * self.prime_list[y]
+                    self.result_matrix[x][y] = result
+                    self.result_matrix[y][x] = result
+
+        return self.result_matrix
--- tests/test_prime_matrix.py
+++ tests/test_prime_matrix.py
@@ -10,5 +10,13 @@ class PrimeMatrixTest(TestCase):
     def test_class(self):
         self.assertIsInstance(self.prime_matrix, PrimeMatrix)
 
+    ''' This test checks the Exceptions '''
+    def test_error_input(self):
+        with self.assertRaises(NotIntegerError):
+            self.prime_matrix.get_matrix(primes="not int")
+        with self.assertRaises(NotSupported):
+            self.prime_matrix.get_matrix(primes=-1)
+
+    ''' Compare result of matrix '''
     def test_get_primes(self):
-        self.assertEqual(self.prime_matrix.get_matrix(10), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+        self.assertTrue((self.prime_matrix.get_matrix(3) == [[2, 3, 5], [3, 9, 15], [5, 15, 25]]).all())
